ff8aff64b0eec349867f95544aa2b58047aa87e2
dicristomanuel <dicristomanuel@icloud.com>
2015-05-27 22:40:59 -0700

    more to do

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ $(document).keypress(function(evt) {
       $(".home").animate({
       	marginTop: "-700px"
       },200)
+       setTimeout(function() {
+					$(".home").remove();
+				}, 300);
 });
 
 $(document).keypress(function(evt) {
@@ -15,33 +18,41 @@ $(document).keypress(function(evt) {
       showMe(keyPressed);
 });
 
-var colors = ["#F75858", "#FA84E3", "#CB84FA", "#84A0FA", "#84FAFA", "#84FAA8", "#BBFA84", "#FAFA84"];
+var colors = ["#BCC7DC", "#C2BCDC", "#DCBCD7", "#DCBCBC", "#F6CD83", "#BEF683", "#83F6B9", "#83F6F4"];
 var index = 0;
+
+function backgroundRandom() {
+	if(!colors[index]) {
+					index = 0;
+				} else {
+				$("body").css({
+					backgroundColor: colors[index]
+				})//cssAnimation
+			index+=1;			
+		}//ifStatement
+	}//backgroundRandom
 
 function showMe(key) {
 	var rand = Math.floor(Math.random()*100)+1;
 	var keyGiven = "_" + key;
 	var thisAnimation = $("<div class='" + keyGiven + "'></div>")
 		if(keyGiven === "_98") {
-			var top = rand + "%";
-			var left = rand + "%";
-			thisAnimation.css({
-				top: top,
-				left: left
-			})//cssAnimation
-		} else if(keyGiven === "_32") {
-			if(!colors[index]) {
-					index = 0;
-				}
-				$("body").css({
-					backgroundColor: colors[index]
+				thisAnimation.css({
+					top: rand + "%",
+					left: rand + "%"
 				})//cssAnimation
-			index+=1;			
-		}//ifStatement
+		} else if(keyGiven === "_32") {
+			backgroundRandom();
+		
 
+		} else if (keyGiven === "_117") {
+					thisAnimation.css({
+						top: rand + "%",
+						left: rand + "%"
+					})//cssAnimation
+				$("body").append(thisAnimation);
+			}//ifStatement
 
-
-	
 	$("body").append(thisAnimation);
 		
 		setTimeout(function() {
