606fbc04e445a197ccea65be79d63084f45c576e
dicristomanuel <dicristomanuel@icloud.com>
2015-05-31 16:27:22 -0700

    working on new animations

--- js/script.js
+++ js/script.js
@@ -37,33 +37,55 @@ function giveMeRandom() {
 	return rand;
 };
 	
+	var leftLast = false;
 
 function showMe(key) {
 	var keyGiven = "_" + key;
-	var thisAnimation = $("<div class='" + keyGiven + "'></div>")
+	var thisAnimation = $("<div class='" + keyGiven + "'></div>");
+
+	console.log(keyGiven);
 		
 	$("." + keyGiven)[0].cloneNode(true).play();
 	$("." + keyGiven).trigger('play');
 
 		if (keyGiven === "_32") {
 			backgroundRandom();
+		
 		} else if (keyGiven === "_100") {
 					thisAnimation.css({
 						top: giveMeRandom() + "%",
 						left: giveMeRandom() + "%"
 					})//cssAnimation
 				$("body").append(thisAnimation);
+			
 			} else if (keyGiven === "_102") {
-					thisAnimation.css({
-						left: giveMeRandom() + "%"
+					if(!leftLast) {
+						thisAnimation.css({
+							left: "68%"
+						})//cssAnimation
+							leftLast = true;
+					} else {
+							thisAnimation.css({
+							left: "0%"
 					})//cssAnimation
+					 leftLast = false;
+						}
 				$("body").append(thisAnimation);
+			
 			} else if (keyGiven === "_104") {
+				  thisAnimation = $('<object><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 726.2 202.9" style="enable-background:new 0 0 726.2 202.9;" xml:space="preserve" class="st0"><rect x="28" y="31.3" width="668.6" height="0.1"/><rect x="28" y="77.3" width="668.6" height="0.1"/><rect x="28" y="123.3" width="668.6" height="0.1"/><rect x="28" y="169.3" width="668.6" height="0.1"/></svg></object>');
+						$("body").append(thisAnimation);
+				} else {
+						$("body").append(thisAnimation);
 				}//ifStatement
-
-	$("body").append(thisAnimation);
-		
 		setTimeout(function() {
 					thisAnimation.remove();
-				}, 1500);
+				}, 2100);
 }//showMe
+
+
+
+
+
+
+
