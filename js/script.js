09e814260de93d4a4d46d4283b1737eac4d71360
dicristomanuel <dicristomanuel@icloud.com>
2015-05-31 22:57:01 -0700

    added custom shape

--- js/script.js
+++ js/script.js
@@ -73,8 +73,26 @@ function showMe(key) {
 				$("body").append(thisAnimation);
 			
 			} else if (keyGiven === "_104") {
+				if(!leftLast) {
 				  thisAnimation = $('<object><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 726.2 202.9" style="enable-background:new 0 0 726.2 202.9;" xml:space="preserve" class="st0"><rect x="28" y="31.3" width="668.6" height="0.1"/><rect x="28" y="77.3" width="668.6" height="0.1"/><rect x="28" y="123.3" width="668.6" height="0.1"/><rect x="28" y="169.3" width="668.6" height="0.1"/></svg></object>');
 						$("body").append(thisAnimation);
+						leftLast = true;
+					} else {
+						thisAnimation = $('<object><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 726.2 202.9" style="enable-background:new 0 0 726.2 202.9;" xml:space="preserve" class="st0 reverse"><rect x="28" y="31.3" width="668.6" height="0.1" stroke="pink"/><rect x="28" y="77.3" width="668.6" height="0.1" stroke="pink"/><rect x="28" y="123.3" width="668.6" height="0.1" stroke="pink"/><rect x="28" y="169.3" width="668.6" height="0.1" stroke="pink"/></svg></object>');
+						$("body").append(thisAnimation);
+						leftLast = false;
+					}
+				} else if (keyGiven === "_106") {
+				
+				if(!leftLast) {
+				  thisAnimation = $('<object><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 446.4 1209.7" style="enable-background:new 0 0 446.4 1209.7;" xml:space="preserve" class="st1"><g id="Layer_1"></g><g id="Layer_2"></g><g id="Layer_4"></g><g id="Layer_3"><polygon points="148.8,1198.7 378.5,800.8 147.4,400.5 375.8,4.8 242.7,4.8 14.2,400.5 14.8,400.5 246,800.9 16.4,1198.7 "/></g></svg></object>');
+						$("body").append(thisAnimation);
+						leftLast = true;
+					} else {
+						thisAnimation = $('<object><svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 446.4 1209.7" style="enable-background:new 0 0 446.4 1209.7;" xml:space="preserve" class="st1"><g id="Layer_1"></g><g id="Layer_2"></g><g id="Layer_4"></g><g id="Layer_3"><polygon points="148.8,1198.7 378.5,800.8 147.4,400.5 375.8,4.8 242.7,4.8 14.2,400.5 14.8,400.5 246,800.9 16.4,1198.7 "/></g></svg></object>');
+						$("body").append(thisAnimation);
+						leftLast = true;
+					}
 				} else {
 						$("body").append(thisAnimation);
 				}//ifStatement
